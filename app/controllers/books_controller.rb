67f3ee329df392cabf3588cb987ab07f7d70d158
Ami-tnk <ami1y251992@gmail.com>
2020-12-16 10:53:54 +0000

    [Update]books#create 変数の確認

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @booknew = Book.new
+    @user = User.find(params[:id])
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -43,7 +46,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 
 end
